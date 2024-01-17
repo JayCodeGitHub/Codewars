06624278ed44aa8e40905f04278a03bcbf5a7d4e
JayCode Developer <JayCode.contakt@gmail.com>
2024-01-17 17:51:05 +0100

    Refactor Total-amount-of-points

--- 8kyu/Total-amount-of-points.js
+++ 8kyu/Total-amount-of-points.js
@@ -1,11 +1,8 @@
 function points(games) {
     let result = 0
     games.forEach((item) => {
-      if(item.charAt(0) > item.charAt(2)) {
-        result += 3;
-      } else if (item.charAt(0) == item.charAt(2)){
-        result += 1;
-      }
+      if(item[0] > item[2]) result += 3;
+      else if (item[0] == item[2]) result += 1;
     })
     return result;
   }
