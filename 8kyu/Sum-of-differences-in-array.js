aaa8aaccdc2703254089a0bc879190ac13130dac
JayCode Developer <JayCode.contakt@gmail.com>
2024-01-17 17:24:47 +0100

    Improve Sum-of-differences-in-array

--- 8kyu/Sum-of-differences-in-array.js
+++ 8kyu/Sum-of-differences-in-array.js
@@ -1,9 +1,8 @@
 function sumOfDifferences(arr) {
     let sum = 0
-    const sorted = arr.sort((x, y) => x - y) 
-    
-    for(i=0; i < sorted.length -1 ; i+=1) {
-        sum += Math.abs(sorted[i] -  sorted[i + 1])
+    arr.sort((x, y) => x - y)
+    for(i=0; i < arr.length -1 ; i+=1) {
+        sum += Math.abs(arr[i] -  arr[i + 1])
     }
     return sum
   }
