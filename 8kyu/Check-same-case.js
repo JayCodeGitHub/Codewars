bdd49e53e1ca2c78878b0e1452d8c367a5ac7131
JayCode Developer <JayCode.contakt@gmail.com>
2024-01-17 17:45:47 +0100

    Refactor Check-same-case

--- 8kyu/Check-same-case.js
+++ 8kyu/Check-same-case.js
@@ -1,8 +1,5 @@
 function sameCase(a, b){
-    const codeA = a.charCodeAt(0);
-    const codeB = b.charCodeAt(0);
-    if(codeA >= 65 && codeA <= 90 && codeB >= 65 && codeB <= 90) return 1;
-    else if((codeA >= 97 && codeA <= 122 && codeB >= 97 && codeB <= 122)) return 1;
-    else if(codeA >= 65 && codeA <= 90 && codeB >= 97 && codeB <= 122 || codeA >= 97 && codeA <= 122 && codeB >= 65 && codeB <= 90) return 0;
-    else return -1;
-  }
+  if (a.toUpperCase() === a.toLowerCase() || b.toUpperCase() === b.toLowerCase()) return - 1;
+  else if(a === a.toLowerCase() && b === b.toLowerCase() ||  a === a.toUpperCase() && b === b.toUpperCase()) return 1;
+  else return 0;
+}
